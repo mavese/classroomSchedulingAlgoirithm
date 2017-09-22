0081c99fc3530c970ae646e2a0f5bfb2873e01ae
mavese <mavese@live.com>
2017-09-21 23:48:51 -0500

    Finished implementation.

--- classroom.py
+++ classroom.py
@@ -12,13 +12,34 @@ def scheduleRooms(rooms,cls):
     If there are not enough rooms to hold the classes, return 'Not enough rooms'.
     """
     rmassign = {}
-    classList = list(sorted(cls,key=cls.get), reverse=True)    
+    classList = list(sorted(cls,key=cls.get, reverse=True))   
     pQ = []
+    empty = False
     heapify(pQ)
     for i in xrange(len(cls)):
-    	clss = classList.pop()
-    	if len(pQ) is 0:
-    		heappush(list(rooms.pop(), cls))#finish
+        if empty:
+            break
+        clss = classList.pop()
+        if len(pQ) is 0:
+            if(len(rooms) is 0):
+                empty = True 
+                continue
+            room = rooms.pop()
+            heappush(pQ, [cls[clss][1], room])
+            rmassign[room] = [clss]
+        elif pQ[0][0] <= cls[clss][0]:
+            room = heappop(pQ)[1]
+            rmassign[room].append(clss)
+            heappush(pQ, [cls[clss][1], room])
+        else:
+            if(len(rooms) is 0): 
+                empty = True
+                continue
+            room = rooms.pop()
+            heappush(pQ, [cls[clss][1], room])
+            rmassign[room] = [clss]
+    if empty:
+        return "Not enough rooms"           
     return rmassign
 
 if __name__=="__main__":
